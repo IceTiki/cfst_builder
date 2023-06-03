c8577b5e35dad25cf205b18d828bad0db2948577
IceTiki <tiki_pufferfish@outlook.com>
2023-06-03 16:33:22 +0800

    ignore test.py

--- test.py
+++ test.py
@@ -1,32 +1,23 @@
 # -*- coding: mbcs -*-
+# print(dir())
+print(__main__)
+print(__package__)
+print(__name__)
+print(__builtins__)
 
 from abaqus import *
 from abaqusConstants import *
 from caeModules import *
 from driverUtils import executeOnCaeStartup
+import urllib2
 
 
-def seqprint(seq):
-    for i in seq:
-        print(i)
+
+def http_get(url):
+    response = urllib2.urlopen(url)
+    content = response.read()
+    return content
 
 
-odb = session.openOdb(
-    name="D:/Environment/Appdata/AbaqusData/Temp/job-2023-4-30--20-38-17.odb"
-)
-step = odb.steps["Step-1"]
-
-with open("tmp.txt", "w") as f:
-    f.write(
-        "\n".join(
-            str(i)
-            for i in step.frames[-1].fieldOutputs["S"].values
-        )
-    )
-
-# for frame in step.frames:
-#     frame_time = frame.frameValue  # 时间
-#     # print(frame.fieldOutputs)
-#     force_values = frame.fieldOutputs["S"].values
-#     seqprint(i for i in force_values)
-#     break
+# ret = http_get("https://www.baidu.com")
+# print(ret)
